283b31407af51cdc6adae87dfc4575094a5d7bea
Nguyen Dinh Hieu <ngdinhhieu1901@gmail.com>
2021-12-28 18:12:58 +0700

    [BaseTest] Updating headless Firefox/Chrome and Internet Explorer, Opera, Coc Coc, Brave browser.

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,10 +1,15 @@
 package commons;
 
 import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 /**
  * <p>
@@ -12,6 +17,9 @@ import org.openqa.selenium.firefox.FirefoxDriver;
  * </p>
  * <ul list-style-type: square;>
  * <li>Used for multiple browser</li>
+ * <li>Used for different OS type such as Windows, MAC, Linux</li>
+ * <li>Used for different device type such as Iphone, Samsung, LG, Sony, Nokia,...</li>
+ * <li>Used for different version of browser (take three lastest version)</li>
  * </ul>
  * 
  * @author Hieu Nguyen
@@ -28,12 +36,40 @@ public class BaseTest {
 		if (browserName.equals("firefox")) {
 			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
 			driver = new FirefoxDriver();
+		} else if (browserName.equals("h_firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+			FirefoxOptions options = new FirefoxOptions();
+			options.addArguments("--headless");
+			options.addArguments("window-size=1920x1080");
+			driver = new FirefoxDriver(options);
 		} else if (browserName.equals("chrome")) {
 			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
 			driver = new ChromeDriver();
+		} else if (browserName.equals("h_chrome")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("-headless");
+			options.addArguments("window-size=1920x1080");
+			driver = new ChromeDriver(options);
 		} else if (browserName.equals("edge")) {
 			System.setProperty("webdriver.edge.driver", projectPath + "\\browserDrivers\\msedgedriver.exe");
 			driver = new EdgeDriver();
+		} else if (browserName.equals("ie")) {
+			System.setProperty("webdriver.ie.driver", projectPath + "\\browserDrivers\\IEDriverServer.exe");
+			driver = new InternetExplorerDriver();
+		} else if (browserName.equals("coccoc")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver_95.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("C:\\Program Files\\CocCoc\\Browser\\Application\\browser.exe");
+			driver = new ChromeDriver(options);
+		} else if (browserName.equals("brave")) {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\browserDrivers\\chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.setBinary("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe");
+			driver = new ChromeDriver(options);
+		} else if (browserName.equals("opera")) {
+			System.setProperty("webdriver.opera.driver", projectPath + "\\browserDrivers\\operadriver.exe");
+			driver = new OperaDriver();
 		} else {
 			throw new RuntimeException("Invalid browser name.");
 		}
